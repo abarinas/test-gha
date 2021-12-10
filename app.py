f752ef2f905d6deaafac9629e62b246d7eecdf06
Jason Alejandro Barinas Guio <alejandro.barinas@autodesk.com>
2021-12-09 19:07:57 -0500

    test region

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import sys
 
 def handler(event, context):
-    return 'Hello aws-ecr-7 from AWS Lambda using Python ' + sys.version + '!'
+    return 'Hello aws-ecr- from AWS Lambda using Python ' + sys.version + '!'
